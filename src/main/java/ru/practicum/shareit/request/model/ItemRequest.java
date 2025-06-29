0e78cfcf3ec906120bb454248346e5e72516a21f
Ruslan Aliullov <aliullov2016@mail.ru>
2025-06-29 12:32:51 +0300

    Выполнение задания спринта 15

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -24,7 +24,7 @@ public class ItemRequest {
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "requester_id", nullable = false)
+    @JoinColumn(name = "requestor_id", nullable = false)
     private User requester;
 
     @Column(nullable = false)
